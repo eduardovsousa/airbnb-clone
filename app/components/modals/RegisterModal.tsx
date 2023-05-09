e9690fe759f529f5014b34f16bef1a4830a0bd60
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-05-09 02:55:55 -0300

    feat: social login (google and github)

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
@@ -84,13 +85,13 @@ const RegisterModal = () => {
         outline
         label="Continue com Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn('google')}
       />
       <Button
         outline
         label="Continue com Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row justify-center items-center gap-2">
