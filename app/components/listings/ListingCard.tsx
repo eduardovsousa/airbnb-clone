428a6cc29c6df23958932f71feb0a9e0b76d135a
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-05-11 01:11:51 -0300

    feat: individual listing view

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Listing, Reservation } from "@prisma/client";
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import { useCallback, useMemo } from "react";
 
 import { useRouter } from "next/navigation";
@@ -12,7 +12,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
