2885987ea9ebc79113ec40a01b2c9c009f254ab9
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-05-15 00:52:29 -0300

    feat: create loader and error page

---
+++ app/error.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface ErrorStateProps {
+  error: Error
+}
+
+const ErrorState: React.FC<ErrorStateProps> = ({
+  error
+}) => {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <EmptyState
+      title="Oh ouh"
+      subtitle="Algo deu de errado!"
+    />
+  )
+};
+
+export default ErrorState;
